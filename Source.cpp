f6e1a947fd3ce18fe5692f3eec952196e52f5c4f
KAK03 <104088666+KAK03@users.noreply.github.com>
2022-07-11 13:33:19 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -7,9 +7,21 @@ using namespace std;
 
 class IDgenerator
 {
+	
+public:
 	int id;
 
-public:
+	IDgenerator()
+	{
+		this->id = 0;
+	}
+
+	IDgenerator(int &id)
+	{
+		this->id = id;
+	}
+
+	/*
 	void getID(int& ID)
 	{
 		cout << id<<endl;
@@ -19,23 +31,34 @@ public:
 	{
 		this->id = ID;
 	}
+	*/
+	
 };
 
 class MAN
 {
+public:
+	
 	string Name;
 	int age, height, weight;
 
-public:
-	/*
 	MAN()
 	{
 		this->Name = nullptr;
 		this->age = 0;
 		this->height = 0;
 		this->weight = 0;
+	}
+
+	MAN(string Name, int age, int height, int weight)
+	{
+		this->Name = Name;
+		this->age = age;
+		this->height = height;
+		this->weight = weight;
 	};
-	*/
+
+	/*
 	void SetInfo(string Name, int age, int height, int weight)
 	{
 		this->Name = Name;
@@ -51,7 +74,42 @@ public:
 		cout << height;
 		cout << weight;
 	}
+	*/
 };
+
+class INFO
+{
+public:
+	string Name;
+	int age, height, weight, id;
+
+	INFO()
+	{
+		this->id = 0;
+		this->Name = nullptr;
+		this->age = 0;
+		this->height = 0;
+		this->weight = 0;
+	}
+};
+
+ostream& operator<<(ostream& os, const IDgenerator& id)//перегрузка операторов ввода и вывода из файла для класса IDgenerator|взаимосвязанные классы ostream,fstream...
+{//описанная для ostream перегрузка распространяется для всех типов данных работы с файлом|первым парамметром передается объект класса(куда записываем)(аналог cout,cin)
+	os << id.id<<" ";
+	return os;
+}
+
+ostream& operator<<(ostream& os, const MAN & man1)
+{
+	os << man1.Name<<" "<<man1.age<<" "<<man1.height<<" " << man1.weight<<"\n";
+	return os;
+}
+
+istream& operator>>(istream& is, INFO& inf)
+{
+	is >> inf.id >> inf.Name >> inf.age >> inf.height >> inf.weight;
+	return is;
+}
 
 int main()
 {
@@ -59,20 +117,19 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	string Name;
+	string Name,stroka;
 	int age, height, weight;
 	int button = 0;
-
+	int i = 1;
 	
-	ofstream fs;
+	fstream fs;
 	fs.exceptions(ifstream::badbit | ifstream::failbit);
-	fs.open("data.txt", fstream::app);//добавление в файл
-	if (fs.is_open())
+	fs.open("data.txt", fstream::in | fstream::out | fstream::app);//чтение и добавление в файл
+	try
 	{
 		do
 		{
 			cout << "Чтобы добавить данные нажмите 1\nЧтобы завершить работу нажмите 2\n";
-			
 			cin >> button;
 			if (button == 1)
 			{
@@ -85,14 +142,18 @@ int main()
 				cout << "Enter your weight:";
 				cin >> weight;
 
-				MAN man;
-				man.SetInfo(Name, age, height, weight);//вводим данные
+				MAN man(Name, age, height, weight);
+				IDgenerator id(i);
 
-				IDgenerator id;
-				int i = 1;
-				id.setID(i);
-				fs.write((char*)&id, sizeof(IDgenerator));
-				fs.write((char*)&man, sizeof(MAN));//передаем указатель чар на ссылку на объект класса MAN, который сохраняем в файл, второй парамметр - это занимаемая им память
+				//INFO inf;
+				//fs >> inf;//выводим инфу из файла в объект класса
+
+				i++;
+				fs << id << " ";//добавляем инфу в файл, через перегруженный оператор
+				fs << man;
+
+				//fs.write((char*)&id, sizeof(IDgenerator));
+				//fs.write((char*)&man, sizeof(MAN));//передаем указатель чар на ссылку на объект класса MAN, который сохраняем в файл, второй парамметр - это занимаемая им память
 			}
 			else if (button == 2)
 			{
@@ -106,35 +167,19 @@ int main()
 		} 
 		while (button == 1);
 	}
-
-	else
+	catch (const ifstream::failure& ex)//вброс ошибки
 	{
 		cout << "ERROR";
 		return 0;
 	}
 	
-
-	/*
-	ifstream fin;
-	fin.open("data.txt");
-	MAN outman;
-	if (fin.is_open())
+	/*//Вывод из файла в строку
+	while (!fs.eof())
 	{
-		cout << "SUCCES";
-		while (fin.read((char*)&outman, sizeof(MAN)))
-		{
-			outman.GetInfo();
-		}
-
-	}
-	else
-	{
-		cout << "ERROR";
-		return 0;
+		getline(fs, stroka);
+		cout << stroka << endl;
 	}
 	*/
 
-	
 	fs.close();
-	//fin.close();
 }
